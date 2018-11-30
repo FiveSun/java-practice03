4eb10f993e5b9f0573764b7753457c690178bd48
FiveSun <FiveSun@TFG155-FIVESUN>
2018-12-01 01:23:01 +0900

    왜 에러나냥

--- src/prob2/Goods.java
+++ src/prob2/Goods.java
@@ -1,5 +1,38 @@
 package prob2;
 
 public class Goods {
+	private String name;
+	private String price;
+	private String count;		
+	
+	public String getName() {
+		return name;
+	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPrice() {
+		return price;
+	}
+
+	public void setPrice(String price) {
+		this.price = price;
+	}
+
+	public String getCount() {
+		return count;
+	}
+
+	public void setCount(String count) {
+		this.count = count;
+	}
+
+
+	
+	public void showInfo() {
+		System.out.println(this.name+"(가격:"+this.price + "원)이 "+this.count + "개 입고 되었습니다.");
+	}
+	
 }
--- src/prob2/GoodsApp.java
+++ src/prob2/GoodsApp.java
@@ -6,21 +6,28 @@ public class GoodsApp {
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
 		Goods[] goods = new Goods[3];
-		
-		/* 입력*/
-		for(int i = 0; i < goods.length; i++) {
+
+		/* 입력 */
+		for (int i = 0; i < goods.length; i++) {
 			String line = scanner.nextLine();
-			String[] infos = line.split(" ");
-			
+			String[] infos = line.split("\\s");
+
+//			goods[i].setName(infos[0]);
+//			goods[i].setPrice(infos[1]);
+//			goods[i].setCount(infos[2]);
+
+			goods[1].setName("Q");
+//			goods[1].setPrice("100");
+//			goods[1].setCount("50");
 		}
-		
+
 		/* 출력 */
-		for(int i = 0; i < goods.length; i++) {
-			String[] tokens = expression.split(" ");
-			
+		for (int i1 = 0; i1 < goods.length; i1++) {
+			goods[i1].showInfo(); //
 		}
 
 		/* 키보드 닫기 */
 		scanner.close();
 	}
+
 }
